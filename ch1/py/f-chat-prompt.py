325a2f5e46740cd4322dba68dd20479e930a7e60
steve woods <steve.woods@m3as.com>
2025-04-02 14:54:12 -0500

    Completed lessons Problem Solution Note
    Signed-off-by: steve woods <m3ac-woodss@m3as.com>
    Signed-off-by: steve woods <steve.woods@m3as.com>

--- ch1/py/f-chat-prompt.py
+++ ch1/py/f-chat-prompt.py
@@ -4,7 +4,9 @@ template = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            'Answer the question based on the context below. If the question cannot be answered using the information provided, answer with "I don\'t know".',
+            '''Answer the question based on the context below. If the
+            question cannot be answered using the information provided, answer with 
+            "I don\'t know".''',
         ),
         ("human", "Context: {context}"),
         ("human", "Question: {question}"),
@@ -13,7 +15,13 @@ template = ChatPromptTemplate.from_messages(
 
 response = template.invoke(
     {
-        "context": "The most recent advancements in NLP are being driven by Large Language Models (LLMs). These models outperform their smaller counterparts and have become invaluable for developers who are creating applications with NLP capabilities. Developers can tap into these models through Hugging Face's `transformers` library, or by utilizing OpenAI and Cohere's offerings through the `openai` and `cohere` libraries, respectively.",
+        "context": '''The most recent advancements in NLP are being driven by Large 
+        Language Models (LLMs). These models outperform their smaller 
+        counterparts and have become invaluable for developers who are creating 
+        applications with NLP capabilities. Developers can tap into these 
+        models through Hugging Face's `transformers` library, or by utilizing 
+        OpenAI and Cohere's offerings through the `openai` and `cohere` 
+        libraries, respectively.''',
         "question": "Which model providers offer LLMs?",
     }
 )
---
+++ run-pgvector.sh
@@ -0,0 +1,7 @@
+docker run \
+    --name pgvector-container \
+    -e POSTGRES_USER=langchain \
+    -e POSTGRES_PASSWORD=langchain \
+    -e POSTGRES_DB=langchain \
+    -p 6024:5432 \
+    -d pgvector/pgvector:pg16
